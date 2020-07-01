68b3dfd965b3952a17a2b00c42a75728d42f1302
iuribittencourt <iuribittencourtti@gmail.com>
2020-07-01 08:57:01 -0300

    Final Project back-end

---
+++ finalproject/src/test/java/iuri/bittencourtpedro/pouzada/finalproject/FinalprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package iuri.bittencourtpedro.pouzada.finalproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
